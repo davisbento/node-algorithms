00af81168a48d277a8914af61210f362a9ee7c9b
davisbento <contato@davisbento.dev>
2023-06-30 13:35:22 -0300

    problem solving - dinner plates

---
+++ problem-solving/dinner-plates/index.spec.ts
@@ -0,0 +1,66 @@
+import { DinnerPlates } from '.';
+
+describe('dinner-plates', () => {
+	test('should initialize the DinnerPlates with empty stack', () => {
+		const dinnerPlates = new DinnerPlates(2);
+		expect(dinnerPlates.getTotalStacks()).toEqual(0);
+	});
+
+	test('should push a value into the stack', () => {
+		const dinnerPlates = new DinnerPlates(2);
+		dinnerPlates.push(1);
+		dinnerPlates.push(2);
+		dinnerPlates.push(3);
+		dinnerPlates.push(4);
+		dinnerPlates.push(5);
+		dinnerPlates.push(6);
+		dinnerPlates.push(7);
+
+		expect(dinnerPlates.getTotalStacks()).toEqual(4);
+		expect(dinnerPlates.getStacks()[0]).toEqual([1, 2]);
+		expect(dinnerPlates.getStacks()[1]).toEqual([3, 4]);
+		expect(dinnerPlates.getStacks()[2]).toEqual([5, 6]);
+		expect(dinnerPlates.getStacks()[3]).toEqual([7]);
+	});
+
+	test('should pop a value from the stack', () => {
+		const dinnerPlates = new DinnerPlates(2);
+		dinnerPlates.push(1);
+		dinnerPlates.push(2);
+		dinnerPlates.push(3);
+		dinnerPlates.push(4);
+		dinnerPlates.push(5);
+		dinnerPlates.push(6);
+		dinnerPlates.push(7);
+
+		const numberRemoved1 = dinnerPlates.pop();
+		const numberRemoved2 = dinnerPlates.pop();
+
+		dinnerPlates.print();
+
+		expect(numberRemoved1).toEqual(7);
+		expect(numberRemoved2).toEqual(6);
+	});
+
+	test('should popstack a value from the stack', () => {
+		const dinnerPlates = new DinnerPlates(2);
+		dinnerPlates.push(1);
+		dinnerPlates.push(2);
+		dinnerPlates.push(3);
+		dinnerPlates.push(4);
+		dinnerPlates.push(5);
+		dinnerPlates.popAtStack(0);
+
+		dinnerPlates.push(20);
+		dinnerPlates.push(21);
+		dinnerPlates.popAtStack(0);
+		dinnerPlates.popAtStack(2);
+		dinnerPlates.pop();
+		dinnerPlates.pop();
+		dinnerPlates.pop();
+		dinnerPlates.pop();
+		dinnerPlates.pop();
+
+		dinnerPlates.print();
+	});
+});
---
+++ problem-solving/dinner-plates/index.ts
@@ -0,0 +1,78 @@
+/*
+You have an infinite number of stacks arranged in a row and numbered (left to right) from 0, each of the stacks has the same maximum capacity.
+
+Implement the DinnerPlates class:
+
+DinnerPlates(int capacity) Initializes the object with the maximum capacity of the stacks capacity.
+void push(int val) Pushes the given integer val into the leftmost stack with a size less than capacity.
+int pop() Returns the value at the top of the rightmost non-empty stack and removes it from that stack, and returns -1 if all the stacks are empty.
+int popAtStack(int index) Returns the value at the top of the stack with the given index index and removes it from that stack or returns -1 if the stack with that given index is empty.
+*/
+
+export class DinnerPlates {
+	private capacity: number = 0;
+	private stacks: number[][] = [];
+
+	constructor(capacity: number) {
+		this.capacity = capacity;
+	}
+
+	private getNextIndexAvailable(): number {
+		const idx = this.stacks.findIndex((stack) => stack.length < this.capacity);
+		return idx;
+	}
+
+	push(val: number): void {
+		const nextIndexAvailable = this.getNextIndexAvailable();
+		if (nextIndexAvailable === -1) {
+			this.stacks.push([val]);
+			return;
+		}
+		this.stacks[nextIndexAvailable].push(val);
+	}
+
+	pop(): number {
+		if (this.stacks.length === 0) {
+			return -1;
+		}
+
+		const lastStackIndex = this.stacks.length - 1;
+
+		let numberRemoved = -1;
+
+		for (let i = lastStackIndex; i >= 0; i--) {
+			const currentStack = this.stacks[i];
+			if (currentStack.length > 0) {
+				const numberRemovedInside = currentStack.pop() || -1;
+				numberRemoved = numberRemovedInside;
+				break;
+			}
+		}
+
+		return numberRemoved;
+	}
+
+	popAtStack(index: number): number {
+		const stackToRemove = this.stacks[index];
+
+		if (stackToRemove === undefined) {
+			return -1;
+		}
+
+		const numberRemoved = stackToRemove.pop() || -1;
+
+		return numberRemoved;
+	}
+
+	print(): void {
+		console.log(this.stacks);
+	}
+
+	public getTotalStacks(): number {
+		return this.stacks.length;
+	}
+
+	public getStacks() {
+		return this.stacks;
+	}
+}
